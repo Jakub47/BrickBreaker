8147e7cd7c892f5cd44bf98cbdcf257bd7aef0d1
Jakub47 <jakub7447@gmail.com>
2019-07-22 23:55:22 +0200

    BrickBreakerV1.0

--- New Unity Project/Assets/Scripts/BallController.cs
+++ New Unity Project/Assets/Scripts/BallController.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     bool canMove;
-    int i = 0;
-    float xPlus;
-    float xMinus;
-    bool rightSide;
+    
+    [SerializeField] float VelocityX;
+    [SerializeField] float VelocityY;
     bool started;
 
     // Start is called before the first frame update
@@ -20,68 +19,78 @@ public class BallController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawRay(transform.position, transform.right,Color.red);
+        float degree = Vector2.Angle(transform.right, transform.up);
+        print(degree);
+        
         Debug.DrawRay(transform.position, transform.up, Color.blue);
 
         if (canMove)
         {
+            transform.Translate(new Vector3(VelocityX, VelocityY, 0));
 
-            transform.Translate(new Vector3(0, .2f, 0), Space.Self);
 
-            //Calculate distance between origin point (tranfosrm.point) and nerest box collider using raycast
-            //Shoot 2 raycast of one x sencond of -x
-            //RaycastHit2D ray1 =  Physics2D.Raycast(transform.position, new Vector2(transform.position.x,transform.position.y));
-            //RaycastHit2D ray2 = Physics2D.Raycast(transform.position, new Vector2(-1 * Mathf.Abs(transform.position.x), transform.position.y));
 
-            RaycastHit2D[] hit1 = Physics2D.RaycastAll(transform.position, transform.right, 100f);
-            RaycastHit2D[] hit2 =  Physics2D.RaycastAll(transform.position, -1 * transform.right, 100f);
 
-            foreach (RaycastHit2D item in hit1)
-            {
-                if (item.collider.gameObject.tag == "Block")
-                {
-                    print(item.transform.gameObject.name);
-                    xPlus = Vector3.Distance(transform.position, item.point);
-                    print("xPlus is " + xPlus);
-                }
-            }
+            //transform.Translate(new Vector3(0, .2f, 0), Space.Self);
 
-            foreach (RaycastHit2D item1 in hit2)
-            {
-                if (item1.collider.gameObject.tag == "Block")
-                {
-                    print(item1.transform.gameObject.name);
-                    xMinus = Vector3.Distance(transform.position, item1.point);
-                    print("xMinus is " + xMinus);
-                }
-            }
+            ////Calculate distance between origin point (tranfosrm.point) and nerest box collider using raycast
+            ////Shoot 2 raycast of one x sencond of -x
+            ////RaycastHit2D ray1 =  Physics2D.Raycast(transform.position, new Vector2(transform.position.x,transform.position.y));
+            ////RaycastHit2D ray2 = Physics2D.Raycast(transform.position, new Vector2(-1 * Mathf.Abs(transform.position.x), transform.position.y));
 
-            if (xPlus > xMinus)
-            {
-                rightSide = true;
-            }
-            else
-                rightSide = false;
+            //RaycastHit2D[] hit1 = Physics2D.RaycastAll(transform.position, transform.right, 100f);
+            //RaycastHit2D[] hit2 =  Physics2D.RaycastAll(transform.position, -1 * transform.right, 100f);
+
+            //foreach (RaycastHit2D item in hit1)
+            //{
+            //    if (item.collider.gameObject.tag == "Block")
+            //    {
+            //        print(item.transform.gameObject.name);
+            //        xPlus = Vector3.Distance(transform.position, item.point);
+            //        print("xPlus is " + xPlus);
+            //    }
+            //}
+
+            //foreach (RaycastHit2D item1 in hit2)
+            //{
+            //    if (item1.collider.gameObject.tag == "Block")
+            //    {
+            //        print(item1.transform.gameObject.name);
+            //        xMinus = Vector3.Distance(transform.position, item1.point);
+            //        print("xMinus is " + xMinus);
+            //    }
+            //}
+
+            //if (xPlus > xMinus)
+            //{
+            //    rightSide = true;
+            //}
+            //else
+            //    rightSide = false;
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.tag == "Block")
+        print(collision.gameObject.name);
+        if (collision.transform.tag == "AxisY")
         {
-            if(rightSide)
-                transform.Rotate(new Vector3(0.0f, 0.0f,(-1 * Mathf.Abs(transform.rotation.z)) - 90f));
-            else
-                transform.Rotate(new Vector3(0.0f, 0.0f, (-1 * Mathf.Abs(transform.rotation.z)) + 90f));
-        }
-        else if (collision.transform.tag == "Player")
-            transform.Rotate(new Vector3(0.0f, 0.0f, (-1 * Mathf.Abs(transform.rotation.z)) + 130f));
-        else if (collision.transform.tag == "pickup")
-        {
-            collision.gameObject.SetActive(false);
-            transform.Rotate(new Vector3(0.0f, 0.0f, (-1 * Mathf.Abs(transform.rotation.z)) + 90f));
+            VelocityY = -1 * VelocityY;
         }
 
+        else if (collision.transform.tag == "AxisMinusY")
+        {
+            VelocityY = -1 * VelocityY;
+        }
+        else if (collision.transform.tag == "AxisX")
+        {
+            VelocityX = -1 * VelocityX;
+
+        }
+        else if (collision.transform.tag == "AxisMinusX")
+        {
+            VelocityX = -1 * VelocityX;
+        }
     }
 
     public void StartMoving()
